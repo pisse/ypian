017f768bb76f0239364597cd28ed3c1ce319912f
huangzhihua <huangzhihua@jd.com>
2017-07-25 15:21:48 +0800

    add filter on taskdetail

--- src/entry/smsbatch/taskdetail.vue
+++ src/entry/smsbatch/taskdetail.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="taskdetail-wrap">
     <div class="search-box">
+      <v-input :formData="formData" :opt="mobile"></v-input>
+      <br>
+      <v-input :formData="formData" :opt="content"></v-input>
+      <br>
+      <v-select :formData="formData" ref="status" :opt="status"></v-select>
+      <br>
+      <v-button text="查询" @search="getList"></v-button>
 
       <div class="record-list">
         <!--<div class="table-operations">
@@ -72,12 +79,52 @@
     mixins: [_request, _pagination],
     data () {
       return {
+        mobile: {
+          name: '手机号',
+          key: 'phone',
+          placeHolder: '请填写正确的手机号'
+        },
+        content: {
+          name: '短信内容',
+          key: 'content',
+          placeHolder: '请填写短信内容'
+        },
+        status: {
+          name: '发送状态',
+          key: 'status',
+          value: '',
+          items: [
+            /* {label: '全部', val: ''},
+             {label: '发送失败', val: '-3'},
+             {label: '任务生成失败', val: '-2'},
+             {label: '不通过', val: '-1'},
+             {label: '待审核', val: '0'},
+             {label: '通过', val: '1'},
+             {label: '任务生成中', val: '2'},
+             {label: '发送中', val: '3'},
+             {label: '发送成功', val: '4'},
+             {label: '部分发送成功', val: '5'} */
+            {label: '全部', val: '100'},
+            {label: '成功', val: '0'},
+            {label: '黑名单', val: '1'},
+            {label: ' 其他错误', val: '2'},
+            {label: ' 最近已发送', val: '3'}
+          ]
+        },
+        formData: {
+          status: '',
+          phone: '',
+          content: ''
+          // phone: '',
+          // start_time: moment().subtract(0, 'days').hour(0).minute(0).second(0).format(dateFormat),
+          // end_time: moment().format(dateFormat)
+        },
         columns: [
           {key: 'send_time', title: '发送时间', width: '180px', formatter: (row, column) => { return moment(new Date(parseInt(row[column['property']] + '000'))).format(dateFormat) }},
           {key: 'send_message', title: '内容', width: '200px', formatter: (row, column) => { return decodeURIComponent(row[column['property']]) }},
           {key: 'fee_count', title: '计费条数'},
           {key: 'phone', title: '手机号'},
-          {key: 'result_type_text', title: '接收状态'}
+          {key: 'result_type_text', title: '发送状态'}
         ],
         tableData: []
       }
@@ -107,7 +154,7 @@
     },
     methods: {
       getList () {
-        let params = {}
+        let params = Object.assign({}, this.formData)
         params['page'] = this.currentPage
         params['task_id'] = this.$route.query.id
 
